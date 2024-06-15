fd0973ba20fd55b6d91c590f73b863525bae0720
Miguel02y <miguelangel19022010@gmail.com>
2024-06-15 10:28:07 -0500

    ultimos ambios

--- middleware/validar-rol-admin.js
+++ middleware/validar-rol-admin.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const validarRolAdmin = { 
     
     validarRolAdmin: (req, res, next) => {
-        if(req.payload.rol != 'Aministrador'){
+        if(req.payload.rol != 'Administrador'){
             return res.status(401).json({ menaje: 'Error unauthorized'})
         }
         next();
